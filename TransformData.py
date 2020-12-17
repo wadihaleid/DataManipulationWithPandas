90b31f82873ceef969eb4e2b07f0eb3b7f1bdc4e
wadihaleid <wadih.aleid@gmail.com>
2020-12-17 14:40:33 +0100

    V1

---
+++ TransformData.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+homelessness = pd.read_csv(r'.\Data\homelessness.csv' )
+
+# Print the head of the homelessness data
+print(homelessness.head())
+
+# Print information about homelessness
+print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
+
+
+# Print the values of homelessness
+print(homelessness.values)
+
+# Print the column index of homelessness
+print(homelessness.columns)
+
+# Print the row index of homelessness
+print(homelessness.index)
+
+
